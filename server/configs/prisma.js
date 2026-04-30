5968dd1791013eec74dc81632a80d7614b8870c9
Rabia gull <gullrabia666@gmail.com>
2026-04-30 23:07:04 +0500

    fix vercel prisma deployment

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,11 +1,21 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client"; 
+import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL,
 });
 
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = globalThis;
 
-export default prisma; 
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
